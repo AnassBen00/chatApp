845f7d48087dc1cfc71060fdbe02737c4a441bd6
Benzekri <benzekrianass@gmail.com>
2024-08-07 12:37:13 +0200

    setting up event listener class configuration

---
+++ src/main/java/com/anass/chatApp/config/WebSocketEventListener.java
@@ -0,0 +1,17 @@
+package com.anass.chatApp.config;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event){
+        // TODO
+    }
+}
